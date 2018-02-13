23246c53d7e2dd37d70fee5ec90c9ae5b98a77fd
FabienMllts <fabienmallet@gmail.com>
2018-02-13 21:24:46 +0100

    Quelques tests qui ne marchent pas

--- GossipDevise/spec/models/user_spec.rb
+++ GossipDevise/spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "devise/registrations/new/html.erb", type: :request do
+	it 'You cannot subscribe without code' do
+  	post '/users', params: {username: "test", email: "test@gmail.com", password: "123456", sign_up_code:""}
+		expect(response.body).to include("Sign up code is not included in the list")
+  end
+
+  it 'You can subscribe with the right code' do
+  	post '/users', params: {username: "coucou", email: "coucou@gmail.com", password: "coucou", sign_up_code:"THP2018"}
+  	expect(response).to have_http_status(200)
+  end
+
+#  it 'redirects subscribed member to index' do
+#  	post '/users', params: {username: "test", email: "test@gmail.com", password: "123456", sign_up_code:"THP2018"}
+# 		expect(response.location).to eq(root_path)
+
+#		expect(response).to redirect_to root_path
+#  end
 end
